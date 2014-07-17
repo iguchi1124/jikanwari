6e7e510c25080bd892590949e968f5f63e2dfb00
iguchi1124 <e.iguchi1124@gmail.com>
2014-07-17 22:33:41 +0900

    Add task management function

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715135829) do
+ActiveRecord::Schema.define(version: 20140716115211) do
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.date     "deadline"
+    t.text     "content"
+    t.integer  "contributor_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
